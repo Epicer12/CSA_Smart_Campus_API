c50e48da34897b406e98f472622bdcebb457e6eb
Epicer12 <hasuntissera12epicer@gmail.com>
2026-04-23 05:20:23 +0530

    Used synchronizedMap method with LinkedHashMaps instead of ConcurrentHashMaps to fix issues with Map object order

--- src/main/java/io/github/epicer12/smart_campus_api/store/DataStore.java
+++ src/main/java/io/github/epicer12/smart_campus_api/store/DataStore.java
@@ -7,17 +7,19 @@ package io.github.epicer12.smart_campus_api.store;
 import io.github.epicer12.smart_campus_api.models.Room;
 import io.github.epicer12.smart_campus_api.models.Sensor;
 import io.github.epicer12.smart_campus_api.models.SensorReading;
+import java.util.Collections;
+import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
-import java.util.concurrent.ConcurrentHashMap;
+
 
 public class DataStore {
     // Rooms Storage
-    public static Map<String, Room> rooms = new ConcurrentHashMap<>();
+    public static Map<String, Room> rooms = Collections.synchronizedMap(new LinkedHashMap<>());
     
     // Sensors Storage
-    public static Map<String, Sensor> sensors = new ConcurrentHashMap<>();
+    public static Map<String, Sensor> sensors = Collections.synchronizedMap(new LinkedHashMap<>());
     
     // Readings Storage
-    public static Map<String, List<SensorReading>> sensorReadings = new ConcurrentHashMap<>();
+    public static Map<String, List<SensorReading>> sensorReadings = Collections.synchronizedMap(new LinkedHashMap<>());
 }
